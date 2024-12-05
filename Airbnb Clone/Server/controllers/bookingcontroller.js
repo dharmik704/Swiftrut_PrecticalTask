c907b4467f9b8e44ba3020ae5acf5256ac45493a
dharmik704 <dharmikbnc@gmail.com>
2024-12-06 03:32:31 +0530

    create cancele booking module

--- Airbnb Clone/Server/controllers/bookingcontroller.js
+++ Airbnb Clone/Server/controllers/bookingcontroller.js
@@ -14,9 +14,10 @@ module.exports.bookproperty = async (req, res) => {
             req.body.userId = req.user._id;
             req.body.startDate = moment(req.body.startDate, "DD/MM/YYYY").format('ll');
             req.body.endDate = moment(req.body.endDate, "DD/MM/YYYY").format('ll');
+            req.body.status = 'Confirmed';
             const booking = await Booking.create(req.body);
             if(booking){
-                return res.status(200).json({ msg: 'Property is successfully bokked', status: 1, response: 'success', BookedProperty: booking });
+                return res.status(200).json({ msg: 'Property is successfully bokked', status: 1, response: 'success', Booked: booking });
             }
             else{
                 return res.status(400).json({ msg: 'Property is not bokked!!', status: 0, response: 'error' });
@@ -24,6 +25,45 @@ module.exports.bookproperty = async (req, res) => {
         }
         else{
             return res.status(400).json({ msg: 'Property is not found!!', status: 0, response: 'error' });
+        }
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(400).json({ msg: 'Somthing went wrong', status: 0, response: 'error' });
+    }
+}
+
+module.exports.getbookedproperty = async (req, res) => {
+    try {
+        const bookproperty = await Booking.find({userId: req.user._id}).sort({_id: -1}).populate('propertyId');
+        if(bookproperty){
+            return res.status(200).json({ msg: 'Your booked Properties', status: 1, response: 'success', BookedProperty: bookproperty });
+        }
+        else{
+            return res.status(400).json({ msg: 'Properties are not founds!!', status: 0, response: 'error' });
+        }
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(400).json({ msg: 'Somthing went wrong', status: 0, response: 'error' });
+    }
+}
+
+module.exports.canclebooking= async (req, res) => {
+    try {
+        const bookproperty = await Booking.findById(req.params.id);
+        if(bookproperty){
+            bookproperty.status = 'Canceled';
+            const cancelebooking = await Booking.findByIdAndUpdate(bookproperty._id, req.body, { new: true}); 
+            if(cancelebooking){
+                return res.status(200).json({ msg: 'Your booking has been canceled', status: 1, response: 'success' });
+            }
+            else{
+                return res.status(200).json({ msg: 'Your booking is not canceled', status: 1, response: 'success' });
+            }
+        }
+        else{
+            return res.status(400).json({ msg: 'Booking not found!!', status: 0, response: 'error' });
         }
     }
     catch (err) {
--- Airbnb Clone/Server/routes/booking.js
+++ Airbnb Clone/Server/routes/booking.js
@@ -4,6 +4,7 @@ const bookingctrl = require('../controllers/bookingcontroller');
 const routes = express.Router();
 
 routes.post('/bookproperty/:id', bookingctrl.bookproperty);
-// routes.get('/getbookproperty', bookingctrl.getbookedproperty);
+routes.get('/getbookproperty', bookingctrl.getbookedproperty);
+routes.put('/canclebooking/:id', bookingctrl.canclebooking);
 
 module.exports = routes;
