700a2a69a6ed9af20d09a11a4d0488911a1ba929
dharmik704 <dharmikbnc@gmail.com>
2024-12-14 00:58:57 +0530

    create getquiz module

--- Quiz App/Backend/controllers/examcontroller.js
+++ Quiz App/Backend/controllers/examcontroller.js
@@ -1,3 +1,18 @@
 const Exam = require('../models/exam.model');
 const Quiz = require('../models/quiz.model');
 
+module.exports.getquiz = async (req, res) => {
+    try {
+        const quiz = await Quiz.find().select('title description question_list');
+        if(quiz){
+            return res.status(200).json({ msg: 'Your Quiz', status: 1, response: 'success', Quiz: quiz });
+        }
+        else{
+            return res.status(400).json({ msg: 'Quiz not found!!', status: 0, response: 'error' });
+        }
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(400).json({ msg: 'Somthing went wrong', status: 0, response: 'error' });
+    }
+}
