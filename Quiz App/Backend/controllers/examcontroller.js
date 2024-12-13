e9b4669f8abd84e58046e2077b57cdda700c1aa2
dharmik704 <dharmikbnc@gmail.com>
2024-12-14 01:29:30 +0530

    create giveExam module

--- Quiz App/Backend/controllers/examcontroller.js
+++ Quiz App/Backend/controllers/examcontroller.js
@@ -16,3 +16,51 @@ module.exports.getquiz = async (req, res) => {
         return res.status(400).json({ msg: 'Somthing went wrong', status: 0, response: 'error' });
     }
 }
+
+module.exports.giveExam = async (req, res) => {
+    try {
+        const quiz = await Quiz.findById(req.params.id);
+        if(!quiz){
+            return res.status(400).json({ msg: 'Quiz not found!!', status: 0, response: 'error' });
+        }
+        
+        let score = 0;
+        const useranswers = req.body.answers;
+
+        const quizanswer = await Quiz.findById(req.params.id).select('answers');
+        quizanswer.answers.forEach((answers, index) => {
+            const useranswer = parseInt(useranswers[index], 10);
+            if(useranswer === answers){
+                score++;
+            }
+        });
+
+        const useraaswers = req.body.answers.map(answer => parseInt(answer, 10));
+
+        if(score < 10){
+            req.body.result = 'Fail'
+        }
+        else{
+            req.body.result = 'Pass'
+        }
+
+        const exam = await new Exam({
+            answers: useraaswers,
+            score,
+            result: req.body.result,
+            userId : req.user._id,
+            quizId: req.params.id
+        })
+        const result = await exam.save();
+        if(result){
+            return res.status(200).json({ msg: 'Exam is successfully submmited', status: 1, response: 'success' });
+        }
+        else{
+            return res.status(400).json({ msg: 'Exam is not submmited!!', status: 0, response: 'error' });
+        }
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(400).json({ msg: 'Somthing went wrong', status: 0, response: 'error' });
+    }
+}
