02e2f9015989f0443bdb9dedd9aab755e2cf1e12
dharmik704 <dharmikbnc@gmail.com>
2024-12-17 00:04:38 +0530

    changes in addsupplier module

--- Inventory Management/Backend/controllers/suppliercntroller.js
+++ Inventory Management/Backend/controllers/suppliercntroller.js
@@ -6,9 +6,7 @@ module.exports.addsupplier = async (req, res) => {
         const inventory = await Inventory.findById(req.params.id);
         if(inventory){
             req.body.role = "Supplier";
-            req.body.userId = req.user._id;
-            req.body.inventoryId = req.params.id;
-            const validemail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+            
             if(!validemail.test(req.body.email)){
                 return res.status(400).json({ msg: 'Invalid email', status: 0, response: 'error' });
             }
