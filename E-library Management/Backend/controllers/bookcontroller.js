de7e1185bb32661f3590597288a8060f370c1b70
dharmik704 <dharmikbnc@gmail.com>
2024-12-19 00:28:28 +0530

    changes in getallbook module

--- E-library Management/Backend/controllers/bookcontroller.js
+++ E-library Management/Backend/controllers/bookcontroller.js
@@ -23,12 +23,7 @@ module.exports.createbook = async (req, res) => {
 module.exports.getallbooks = async (req, res) => {
     try {
         const allbooks = await Book.find().sort({_id: -1});
-        if(allbooks){
-            return res.status(200).json({ msg: 'Your all Books', status: 1, response: 'success', AllBooks: allbooks });
-        }
-        else{
-            return res.status(400).json({ msg: 'Books are not founds!!', status: 0, response: 'error' });
-        }
+        
     }
     catch (err) {
         console.log(err);
@@ -40,14 +35,7 @@ module.exports.updatbook = async (req, res) => {
     try {
         const bookdata = await Book.findById(req.params.id);
         if(bookdata){
-            req.body.updateAt = moment().format('LLL');
-            const updatebook = await Book.findByIdAndUpdate(req.params.id, req.body, {new: true});
-            if(updatebook){
-                return res.status(200).json({ msg: 'Book is updated successfully', status: 1, response: 'success', UpdatedBook: updatebook });
-            }
-            else{
-                return res.status(400).json({ msg: 'Book is not updated!!', status: 0, response: 'error' });
-            }
+            
         }
         else{
             return res.status(400).json({ msg: 'Book is not found!!', status: 0, response: 'error' });
